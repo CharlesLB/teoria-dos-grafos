0bc292e861eb70769385448ab39f511ded685dee
CharlesLB <charles.braga.jf.cb@gmail.com>
2024-09-29 12:27:58 -0300

    feat: Criar grafo com .txt

--- src/core/Main/main.cpp
+++ src/core/Main/main.cpp
@@ -16,27 +16,64 @@
 
 using namespace std;
 
-int Main::main(int argc, char* argv[]) {
-    string path, inputPath, outputPath;
-    bool directed, weightedEdge, weightedNode;
-
-    if (argc <= 5) {
+int Main::main(int argc, char *argv[]) {
+    if (argc < 2) {
         cout << "Missing arguments.\n";
-        cout << "Usage: " << argv[0] << " <input_file>"
-             << " <output_file>"
-             << " <directed[0,1]> <weightedEdge[0,1]> <weightedNode[0,1]>"
-             << endl;
+        cout << "Usage: " << argv[0] << " <input_file> [output_file] [directed] [weightedEdge] [weightedNode]" << endl;
         return 1;
     }
 
-    inputPath = argv[1];
-    outputPath = argv[2];
-    directed = atoi(argv[3]);
-    weightedEdge = atoi(argv[4]);
-    weightedNode = atoi(argv[5]);
+    string inputPath = argv[1];
 
-    Graph* graph = Reader::graph(inputPath, directed, weightedEdge, weightedNode);
+    if (inputPath.find(".dat") != string::npos) {
+        return DATProcess(argc, argv);
+    } else if (inputPath.find(".txt") != string::npos) {
+        return AMPLProcess(argc, argv);
+    } else {
+        cout << "Unsupported file type. Only .dat and .txt files are supported.\n";
+        return 1;
+    }
+}
+
+int Main::DATProcess(int argc, char *argv[]) {
+    if (argc < 5) {
+        cout << "Missing arguments for .dat file.\n";
+        cout << "Usage: " << argv[0] << " <output_file> <directed[0,1]> <weightedEdge[0,1]> <weightedNode[0,1]>" << endl;
+        return 1;
+    }
+
+    string inputPath = argv[1];
+    string outputPath = argv[2];
+    bool directed = atoi(argv[3]);
+    bool weightedEdge = atoi(argv[4]);
+    bool weightedNode = atoi(argv[5]);
+
+    Graph *graph = Reader::graph(inputPath, directed, weightedEdge, weightedNode);
+    if (!graph) {
+        cout << "Error reading graph from file " << inputPath << endl;
+        return 1;
+    }
+
     Controller::processOperation(argv, weightedNode, weightedEdge, directed, graph);
+
+    return 0;
+}
+
+int Main::AMPLProcess(int argc, char *argv[]) {
+    if (argc < 2) {
+        cout << "Missing arguments for .txt file.\n";
+        cout << "Usage: " << argv[0] << " <output_file>" << endl;
+        return 1;
+    }
+
+    string inputPath = argv[1];
+    string outputPath = argv[2];
+
+    Graph *graph = Reader::graphAMPL(inputPath);
+    if (!graph) {
+        cout << "Error reading graph from file " << inputPath << endl;
+        return 1;
+    }
 
     return 0;
 }
--- src/core/Reader/reader.cpp
+++ src/core/Reader/reader.cpp
@@ -5,6 +5,7 @@
 #include <cstring>
 #include <fstream>
 #include <iostream>
+#include <set>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -12,7 +13,7 @@
 #include "../../lib/Graph/graph.hpp"
 
 vector<string> Reader::readLines(string filename) {
-    FILE* file = openFile(filename);
+    FILE *file = openFile(filename);
     vector<string> lines;
 
     char buffer[256];
@@ -25,11 +26,11 @@ vector<string> Reader::readLines(string filename) {
     return lines;
 }
 
-Graph* Reader::graph(string filename, bool directed, bool weightedEdges, bool weightedNodes) {
-    FILE* file = openFile(filename);
+Graph *Reader::graph(string filename, bool directed, bool weightedEdges, bool weightedNodes) {
+    FILE *file = openFile(filename);
     vector<string> lines;
 
-    Graph* graph = new Graph(directed, weightedEdges, weightedNodes);
+    Graph *graph = new Graph(directed, weightedEdges, weightedNodes);
 
     lines = readLines(filename);
     int totalNodes = stoi(lines[0]);
@@ -41,13 +42,13 @@ Graph* Reader::graph(string filename, bool directed, bool weightedEdges, bool we
     lines.erase(lines.begin());
 
     if (weightedEdges) {
-        for (const string& line : lines) {
+        for (const string &line : lines) {
             istringstream iss(line);
             int source, destination, weight;
             iss >> source >> destination >> weight;
 
-            Node* sourceNode = graph->findNodeById(source);
-            Node* destinationNode = graph->findNodeById(destination);
+            Node *sourceNode = graph->findNodeById(source);
+            Node *destinationNode = graph->findNodeById(destination);
 
             graph->createEdge(sourceNode, destinationNode, weight);
         }
@@ -55,13 +56,13 @@ Graph* Reader::graph(string filename, bool directed, bool weightedEdges, bool we
         return graph;
     }
 
-    for (const string& line : lines) {
+    for (const string &line : lines) {
         istringstream iss(line);
         int source, destination;
         iss >> source >> destination;
 
-        Node* sourceNode = graph->findNodeById(source);
-        Node* destinationNode = graph->findNodeById(destination);
+        Node *sourceNode = graph->findNodeById(source);
+        Node *destinationNode = graph->findNodeById(destination);
 
         graph->createEdge(sourceNode, destinationNode, 1);
     }
@@ -69,8 +70,8 @@ Graph* Reader::graph(string filename, bool directed, bool weightedEdges, bool we
     return graph;
 }
 
-FILE* Reader::openFile(string filename) {
-    FILE* file = fopen(filename.c_str(), "r");
+FILE *Reader::openFile(string filename) {
+    FILE *file = fopen(filename.c_str(), "r");
 
     if (file == NULL) {
         cout << "Error opening file " << filename << endl;
@@ -106,6 +107,78 @@ int Reader::readInt() {
     }
 
     return num;
+}
+
+Graph *Reader::graphAMPL(string filename) {
+    GraphAMPL graphAMPL = getDataFromAMPL(filename);
+
+    Graph *graph = new Graph(false, true, true);
+
+    for (int i = 1; i <= graphAMPL.numClusters; i++) {
+        graph->createOrUpdateNode(i, 1);
+    }
+
+    for (int i = 0; i < graphAMPL.nodeWeights.size(); i++) {
+        graph->createOrUpdateNode(graphAMPL.nodeWeights[i].first, graphAMPL.nodeWeights[i].second);
+    }
+
+    for (int i = 0; i < graphAMPL.edges.size(); i++) {
+        Node *sourceNode = graph->findNodeById(graphAMPL.edges[i].first);
+        Node *destinationNode = graph->findNodeById(graphAMPL.edges[i].second);
+
+        if (sourceNode == nullptr || destinationNode == nullptr) {
+            continue;
+        }
+
+        graph->createEdge(sourceNode, destinationNode, 1);
+    }
+
+    return graph;
+}
+
+GraphAMPL Reader::getDataFromAMPL(string filename) {
+    ifstream file(filename);
+    GraphAMPL graphAMPL;
+
+    string line;
+
+    while (getline(file, line)) {
+        if (line.find("param p :=") != string::npos) {
+            stringstream ss(line);
+            string temp;
+            ss >> temp >> temp >> temp >> graphAMPL.numClusters;
+        } else if (line.find("set V :=") != string::npos) {
+            getline(file, line);
+            stringstream ss(line);
+            int vertex;
+            while (ss >> vertex) {
+                graphAMPL.nodes.insert(vertex);
+            }
+        } else if (line.find("param w :=") != string::npos) {
+            while (getline(file, line) && !line.empty() && line.find(";") == string::npos) {
+                stringstream ss(line);
+                int vertex, weight;
+                ss >> vertex >> weight;
+                graphAMPL.nodeWeights.push_back({vertex, weight});
+            }
+        } else if (line.find("set E :=") != string::npos) {
+            while (getline(file, line) && !line.empty() && line.find("end;") == string::npos) {
+                stringstream ss(line);
+                char ignore;
+                int v1, v2;
+                while (ss >> ignore >> v1 >> ignore >> v2 >> ignore) {
+                    graphAMPL.edges.push_back({v1, v2});
+                }
+            }
+        } else if (line.find("set Y0 :=") != string::npos) {
+            while (getline(file, line) && line.find(");") == string::npos) {
+                // Do nothing
+            }
+        }
+    }
+
+    file.close();
+    return graphAMPL;
 }
 
 void Reader::continueConfirmation() {
